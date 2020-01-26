129c07962d77087ce08ee4c024c61cd8dc791acb
Blaise <blaiseappo@gmail.com>
2020-01-26 13:46:18 -0800

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Macuser on 2020-01-26.
+//  Copyright © 2020 Blaise. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
